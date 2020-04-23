561a187404afd181fac5453f8bc80362e935ec76
ybark <kevindoughlife.com>
2020-04-23 16:44:38 -0400

    _2_login.jav

--- src/tests/_2_login.java
+++ src/tests/_2_login.java
@@ -3,5 +3,6 @@ package tests;
 public class _2_login {
     public static void main(String[] args) {
         System.out.println("2nd file");
+        System.out.println("It will be easy");
     }
 }
